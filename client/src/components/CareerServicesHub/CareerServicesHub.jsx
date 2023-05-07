39409d0889691145a6a6f1ed03ee7ea143b0f560
Mason Money <moneymason0@gmail.com>
2023-05-07 16:04:27 -0600

    Finished main capability for import students button

--- client/src/components/CareerServicesHub/CareerServicesHub.jsx
+++ client/src/components/CareerServicesHub/CareerServicesHub.jsx
@@ -2,26 +2,25 @@ import React, {useContext} from 'react';
 import { EventsContext } from '../../context/eventsContext';
 import { StudentsContext } from '../../context/studentsContext';
 import { ManagersContext } from '../../context/managersContext';
-import StudentCardsList from './StudentCardsList';
 
+import StudentCardsList from './StudentCardsList';
 import ExcelImportButton from './Excel Import Button/ExcelImportButton'
+
+import './CareerServicesHub.css'
 
 export default function CareerServicesHub() {
 
     const eventContext = useContext(EventsContext);
     const events = eventContext.eventsData;
-    console.log(events);
 
     const studentContext = useContext(StudentsContext);
     const students = studentContext.studentsData;
-    console.log(students);
 
     const managersContext = useContext(ManagersContext);
     const managers = managersContext.managersData;
-    console.log(managers);
 
   return (
-    <div>
+    <div className='main-body-container'>
       <StudentCardsList/>
       < ExcelImportButton />
     </div>
--- client/src/components/CareerServicesHub/StudentCardsList.jsx
+++ client/src/components/CareerServicesHub/StudentCardsList.jsx
@@ -7,23 +7,19 @@ import './StudentCardList.css'
 export default function StudentCardslist () {
 const studentContext = useContext(StudentsContext);
 const students = studentContext.studentsData;
-console.log(students);
-
-
     
     return (
         <>
-<div className='student-card-container'>
-    {students != null ? students.map((student) => {
-        console.log(student);
-    return(
-        <div key={student.student_id}>
-            <StudentCard  student= {student}/>
-        </div>
-    )
-    }) : "Loading"}
-</div>
-</>
+            <div className='student-card-container'>
+            {students != null ? students.map((student) => {
+            return(
+                <div key={student.student_id}>
+                    <StudentCard  student= {student}/>
+                </div>
+            )
+            }) : "Loading"}
+            </div>
+        </>
     )
 }
 
