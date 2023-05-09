3895465af1d914939ef78df9c920baac89956ad9
Never404s <kdsossa@gmail.com>
2023-05-09 18:32:47 -0500

    plz work

--- api/src/db/migration.js
+++ api/src/db/migration.js
@@ -2,30 +2,26 @@ import pg from "pg";
 
 const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
+// Delay function
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function migration() {
-    await db.query(`DROP TABLE IF EXISTS milestone CASCADE`, (err, data) => {
-        if (err) {
-            console.log('Drop tables failed', err);
-        }
-        db.query(`CREATE TABLE milestone (
+    // Milestone table
+    await db.query(`DROP TABLE IF EXISTS milestone CASCADE`);
+    await db.query(`CREATE TABLE milestone (
             id SERIAL PRIMARY KEY NOT NULL,
             mile_name TEXT NOT NULL,
             progress_stat TEXT NOT NULL,
             student_id INTEGER REFERENCES student (id)
-        )`, (err, data) => {
-            if(err){
-                console.log('Create Milestone table failed', err);
-            }else{
-                console.log('Create Milestone table successful');
-            }
-        })
-    });
+        )`);
+    console.log('Milestone table created');
+    await delay(2000); // 2-second delay
 
-    await db.query(`DROP TABLE IF EXISTS service_manager CASCADE`, (err, data) => {
-        if(err){
-            console.log('TSCM table drop failure');
-        }
-        db.query(`CREATE TABLE service_manager (
+    // Service_manager table
+    await db.query(`DROP TABLE IF EXISTS service_manager CASCADE`);
+    await db.query(`CREATE TABLE service_manager (
             id SERIAL PRIMARY KEY NOT NULL,
             tscm_first VARCHAR(50) NOT NULL,
             tscm_last VARCHAR(50) NOT NULL,
@@ -33,21 +29,13 @@ async function migration() {
             tscm_password text NOT NULL,
             tscm_email text NOT NULL,
             tscm_avatar text NOT NULL
-        )`, (err, data) => {
-            if(err){
-                console.log('Create TSCM table failed', err);
-            }else{
-                console.log('TSCM table created');
-            }
-        })
-    });
+        )`);
+    console.log('TSCM table created');
+    await delay(2000); // 2-second delay
 
-    await db.query (`DROP TABLE IF EXISTS student CASCADE`, (err, data) => {
-        if (err){
-            console.log('Student table failure', err);
-        }
-
-        db.query(`CREATE TABLE student (
+    // Student table
+    await db.query(`DROP TABLE IF EXISTS student CASCADE`);
+    await db.query(`CREATE TABLE student (
             id SERIAL PRIMARY KEY NOT NULL,
             student_first VARCHAR(50) NOT NULL,
             student_last VARCHAR(50) NOT NULL,
@@ -56,22 +44,13 @@ async function migration() {
             career_status VARCHAR(50) NOT NULL,
             course_status VARCHAR(50) NOT NULL,
             tscm_id INTEGER NOT NULL REFERENCES service_manager (id)
-        )`, (err, data) => {
-            if(err){
-                console.log('Create Student Table failed', err);
-            }else{
-                console.log('Student table created successfully');
-            }
-        })
+        )`);
+    console.log('Student table created');
+    await delay(2000); // 2-second delay
 
-    });
-
-    await db.query (`DROP TABLE IF EXISTS calendar CASCADE`, (err, data) => {
-        if(err){
-            console.log('Calendar table failure', err);
-        }
-
-        db.query(`CREATE TABLE calendar (
+    // Calendar table
+    await db.query(`DROP TABLE IF EXISTS calendar CASCADE`);
+    await db.query(`CREATE TABLE calendar (
             id SERIAL PRIMARY KEY NOT NULL,
             event_name VARCHAR(50) NOT NULL,
             tscm_id INTEGER NOT NULL REFERENCES service_manager (id),
@@ -79,15 +58,8 @@ async function migration() {
             event_time TIME NOT NULL,
             speak_con VARCHAR(50) NOT NULL,
             event_descrip text NOT NULL
-        )`, (err, data) => {
-            if(err){
-                console.log('Create Calendar Table failed', err);
-            }else{
-                console.log('Create Calendar Table successful');
-            }
-        })
-
-    });
+        )`);
+    console.log('Calendar table created');
 }
 
 migration();
