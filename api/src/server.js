265e435b7151fcf127f02f157d05397499d843f6
Never404s <kdsossa@gmail.com>
2023-05-09 16:33:22 -0500

    server adjustments for deployment

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,8 @@
-import express from "express";
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
 
-import pg from "pg";
+const db = new Pool({ connectionString: process.env.DATABASE_URL });
 
 const PORT = process.env.PORT
 
@@ -11,25 +13,149 @@ app.use(bodyParser.json());
 
 app.use(express.json());
 
-app.get("/api/tasks", async (req, res, next) => {
-  const result = await db.query("SELECT * FROM tasks").catch(next);
-  res.send(result.rows);
-});
+app.use(
+  cors({
+    origin:"*"
+  })
+);
 
-app.get("/api/tasks/:id", async (req, res, next) => {
+// --------------------------------------------- STUDENT ROUTES ----------------------------------------------------------------------------
+app.get("/students", async (req, res, next) =>{
+  const results = await db.query(`SELECT student.*, service_manager.tscm_first, service_manager.tscm_last
+                                  FROM student 
+                                  JOIN service_manager ON service_manager.tscm_id = student.tscm_id`)
+                          .catch(next);
+  res.send(results.rows)
+})
+
+app.get("/students/:id", async (req, res, next) =>{
+  const id = req.params.id;
+
+  const result = await db.query(`SELECT student.*, service_manager.tscm_first, service_manager.tscm_last 
+                                  FROM student 
+                                  JOIN service_manager ON service_manager.tscm_id = student.tscm_id 
+                                  WHERE student.student_id = ${id}`)
+                            .catch(next);
+
+                  if (result.rows.length === 0) {
+                        res.sendStatus(404);
+                      } else {
+                        res.send(result.rows);
+                      }
+})
+
+app.post("/students", async (req, res, next) => {
+  const firstName = req.body.student_first;
+  const lastName = req.body.student_last;
+  const cohort = req.body.cohort;
+  const sercurityClearance = req.body.sec_clearance;
+  const careerStatus = req.body.career_status;
+  const courseStatus = req.body.course_status;
+  const tscm_id = req.body.tscm_id;
+
   const result = await db
-    .query("SELECT * FROM tasks WHERE id = $1", [req.params.id])
+    .query("INSERT INTO student(student_first, student_last, cohort, sec_clearance, career_status, course_status, tscm_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *", [firstName, lastName, cohort, sercurityClearance, careerStatus, courseStatus, tscm_id])
     .catch(next);
+  res.send(result.rows[0]);
+})
 
-  if (result.rows.length === 0) {
-    res.sendStatus(404);
-  } else {
-    res.send(result.rows[0]);
-  }
-});
+app.patch("/students/:id", async (req, res, next) => {
+  const id = req.params.id;
 
-app.post("/api/tasks", async (req, res, next) => {
-  const { description } = req.body;
+  const firstName = req.body.student_first;
+  const lastName = req.body.student_last;
+  const cohort = req.body.cohort;
+  const sercurityClearance = req.body.sec_clearance;
+  const careerStatus = req.body.career_status;
+  const courseStatus = req.body.course_status;
+  const tscm_id = req.body.tscm_id;
+
+  const result = await db
+    .query("UPDATE student SET student_first = $1, student_last = $2, cohort = $3, sec_clearance = $4, career_status = $5, course_status = $6, tscm_id = $7 WHERE student_id = $8 RETURNING *", [firstName, lastName, cohort, sercurityClearance, careerStatus, courseStatus, tscm_id, id])
+    .catch(next);
+  res.send(result.rows[0]);
+})
+
+app.delete("/students/:id", async (req, res, next) => {
+  const id = req.params.id;
+
+   await db.query("DELETE FROM milestone WHERE milestone.student_id = $1", [id])
+
+   await db.query("DELETE FROM student WHERE student.student_id = $1", [id])
+    .catch(next);
+  res.send('Sucessfully Deleted Student Record!');
+})
+
+// --------------------------------------------- MILESTONE ROUTES ----------------------------------------------------------------------------
+
+app.get("/students/:id/milestones", async (req, res, next) =>{
+  const id = req.params.id;
+
+  const result = await db.query(`SELECT * FROM milestone WHERE milestone.student_id = ${id}`)
+                            .catch(next);
+  res.send(result.rows);
+})
+
+app.post("/students/:studentId/milestones", async (req, res, next) =>{
+  const studentId = req.params.studentId;
+
+  const mileName = req.body.mile_name;
+  const progress = req.body.progress_stat;
+
+  const result = await db.query(`INSERT INTO milestone (mile_name, progress_stat, student_id ) VALUES( $1, $2, $3 ) RETURNING *`, [mileName, progress, studentId])
+                            .catch(next);
+  res.send(result.rows);
+})
+
+app.patch("/students/:studentId/milestones/:milestoneId", async (req, res, next) =>{
+  const milestoneId = req.params.milestoneId;
+  const studentId = req.params.studentId;
+
+  const mileName = req.body.mile_name;
+  const progress = req.body.progress_stat;
+
+  const result = await db.query(`UPDATE milestone SET mile_name = $1, progress_stat = $2, student_id = $3 WHERE milestone.mile_id = $4 RETURNING *`, [mileName, progress, studentId, milestoneId])
+                            .catch(next);
+  res.send(result.rows);
+})
+
+// --------------------------------------------- MANAGERS ROUTES ----------------------------------------------------------------------------
+
+app.get ("/managers", async (req, res, next) => {
+  const results = await db.query(`SELECT * FROM service_manager`).catch(next);
+  res.send(results.rows)
+})
+
+app.get ("/managers/:id", async (req, res, next) => {
+  const id = req.params.id;
+  
+  const results = await db.query(`SELECT * FROM service_manager WHERE tscm_id = ${id}`).catch(next);
+  res.send(results.rows)
+})
+
+app.post("/managers", async (req, res, next) => {
+  const firstName = req.body.tscm_first;
+  const lastName = req.body.tscm_last;
+  const login_id = req.body.login_id;
+  const password = req.body.tscm_password;
+  const email = req.body.tscm_email;
+  const avatar = req.body.tscm_avatar;
+
+  const result = await db
+    .query("INSERT INTO service_manager(tscm_first, tscm_last, login_id, tscm_password, tscm_email, tscm_avatar) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *", [firstName, lastName, login_id, password, email, avatar])
+    .catch(next);
+  res.send(result.rows[0]);
+})
+
+app.patch("/managers/:id", async (req, res, next) => {
+  const id = req.params.id;
+
+  const firstName = req.body.tscm_first;
+  const lastName = req.body.tscm_last;
+  const login_id = req.body.login_id;
+  const password = req.body.tscm_password;
+  const email = req.body.tscm_email;
+  const avatar = req.body.tscm_avatar;
 
   const result = await db
     .query("UPDATE service_manager SET tscm_first = $1, tscm_last = $2, login_id = $3, tscm_password = $4, tscm_email = $5, tscm_avatar = $6  WHERE tscm_id = $7 RETURNING *", [firstName, lastName, login_id, password, email, avatar, id])
@@ -106,16 +232,37 @@ app.post("/events", async (req, res, next) => {
   res.send(result.rows[0]);
 });
 
-app.delete("/api/tasks/:id", async (req, res, next) => {
-  const { id } = req.params;
+app.patch("/events/:id", async (req, res, next) => {
+  const id = req.params.id;
 
-  await db.query("DELETE FROM tasks WHERE id = $1", [id]).catch(next);
-  res.sendStatus(204);
-});
+  const eventName = req.body.event_name;
+  const tscmId = req.body.tscm_id;
+  const date = req.body.event_date;
+  const time = req.body.event_time;
+  const speakersContacted = req.body.speak_con;
+  const description = req.body.event_descrip;
+
+  const result = await db
+    .query("UPDATE calendar SET event_name = $1, tscm_id = $2, event_date = $3, event_time = $4, speak_con = $5, event_descrip = $6 WHERE event_id = $7 RETURNING *", [eventName, tscmId, date, time, speakersContacted, description, id])
+    .catch(next);
+  res.send(result.rows[0]);
+})
+
+app.delete("/events/:id", async (req, res, next) => {
+  const id = req.params.id;
+
+  await db.query("DELETE FROM calendar WHERE calendar.event_id = $1", [id])
+    .catch(next);
+  res.send('Sucessfully Deleted Event Record!');
+})
+
+// -----------------------------------------------------------------------------------------------------------------------------------------
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Internal Server Error");
 });
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}00!`);
+});
